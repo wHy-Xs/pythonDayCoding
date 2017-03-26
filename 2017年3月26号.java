7dbb6a037165bbfaef3038445436f0dd28163e30
wHy-Xs <603987690@qq.com>
2017-03-26 15:19:34 +0800

    Create 2017年3月26号.java

---
+++ 2017年3月26号.java
@@ -0,0 +1,46 @@
+1.两个字符串链接两种方法：
+  1.String a="absd";
+    String b="dfdf";
+    String c=a+b;
+  2.String a="dfd";
+    String c=a.concat("dfdfdf");
+
+2.替换字符串
+  String中的replace(old,new);replaceAll(regx，用什么替换regx匹配到的字符）;replaceFirst(regx,tongli)
+
+3.求一个无序数组中第K大或第k小的数（可以）重复
+  public static void main(String[]args){
+    Scanner cin=new Scanner(System.in);
+    int k=cin.nextInt();
+    //int[]arr=new int[10];
+    int[]a={1,2,3,4,4,5,6,6,3,2,1,4,};
+    //排序
+    for(int i=0;i<arr.length;i++){
+      for(int j=0;j<arr.length-i-1;j++){
+        if(arr[j]>arr[j+1]){
+          int tmp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=tmp;
+        }
+      }
+    }
+    
+    int chongfuShu=0;
+    int danshuchongfu=0;
+    int[]arg=new int[arr.length];
+    for(int m=0;m<arr.length;m++){
+      for(int n=m+1;n<arr.length;n++){
+        if(arr[m]==arg[n]){
+          chongfushu++;
+          danshuchongfu=m;
+          m=m+1;
+          continue;
+        }
+      }
+     arg[m]=arr[m];
+    }
+  }
+/*
+求一个无序序列中第K大/小个数，如果没有重复元素，则直接排序，找到第K大个数就行，但是如果有重复的元素，就比较麻烦。没有搁进去的值，java会自动赋值0.
+所以只能排序。
+*/
