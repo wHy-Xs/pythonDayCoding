38d38ba77495eba4c1c0300c60194ec0f8c0d621
wHy-Xs <603987690@qq.com>
2017-06-15 18:25:53 +0800

    Create Coding.py

--- Coding.py
+++ Coding.py
@@ -59,3 +59,18 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+4.最大公约数与最小公倍数：解法：用辗转相除法先求出最大公约数，再通过公式：两个数相乘等于这两个数的最大公约数和最小公倍数的积。求出最小公倍数
+    public static int commonDivisor(int n,int m){
+    #辗转相除是用大的除以小的。如果n<m，第一次相当n与m值交换
+    while(n%m!=0){
+    int temp=n%m;
+    n=m;
+    m=temp;
+    }
+    return m;
+    }
+    #求最小公倍数
+    public static int commonMultiple(int n,int m){
+    return n*m/commonDivisor(n,m);
+}
