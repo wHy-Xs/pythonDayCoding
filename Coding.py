c6076f75bf4130094f229016c173e2662f3ca540
wHy-Xs <603987690@qq.com>
2017-06-15 18:44:56 +0800

    Create Coding.py

--- Coding.py
+++ Coding.py
@@ -107,3 +107,17 @@ if __name__ == '__main__':
 				
 			}
 }
+
+6.判断字符串是否没有重复字符.实现一个算法确定字符串中的字符是否均唯一出现
+    class Solution:
+        # @param s: a string
+        # @return: a boolean
+        def isUnique(self, str):
+            # write your code here
+            d={}
+            for i in str:
+                if i not in d:
+                    d[i]=1
+                else:
+                    return False
+return True
