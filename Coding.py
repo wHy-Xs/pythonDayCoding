6048e16c8750d20082724b11144049984f2d096c
wHy-Xs <603987690@qq.com>
2017-06-19 09:33:56 +0800

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -121,3 +121,11 @@ if __name__ == '__main__':
                 else:
                     return False
 return True
+7.递归实现阶乘
+def refunc(n):
+	i=1
+	if n>1:
+	  i=n
+	  n=n*refunc(n-1)
+        print "% d!="%i,n
+        return n
