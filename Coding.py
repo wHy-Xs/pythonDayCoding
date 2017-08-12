55fa98a188f2b137b8c0ff234a13c3dd8a351fa5
wHy-Xs <603987690@qq.com>
2017-08-12 18:05:47 +0800

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -614,3 +614,49 @@ def addTwoNumbers(self, l1, l2):
 	    break  
 
     return ret  
+10.链表翻转
+方法一：利用栈结构，将链表内容依次压入栈，再从栈依次弹出即可构造逆序。下面的代码用普通数组模拟栈。
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p = head
+        newList = []
+        while p:
+            newList.insert(0, p.val)
+            p = p.next
+
+        p = head
+        for v in newList:
+            p.val = v
+            p = p.next
+        return head
+
+与思路一栈的思想类似，不过是直接在原链表操作，通过迭代将节点重组，前面的节点转移到重组链表的后面。实际上就是头结点倒插操作。
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        new_head = None
+        while head:
+            p = head
+            head = head.next
+            p.next = new_head
+            new_head = p
+        return new_head
